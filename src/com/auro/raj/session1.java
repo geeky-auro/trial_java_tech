46bf97adcc75b17b05c2d730db4defff05f2a978
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-01 23:57:54 +0530

    assignment 1

--- src/com/auro/raj/session1.java
+++ src/com/auro/raj/session1.java
@@ -6,6 +6,10 @@ public class session1 {
     public static void main(String args[]){
 //        Average of 3 no's ..!
 
+        int a=10,b=5,c=10;
+        double avg=(a+b+c)/3.0;
+        System.out.println(avg);
+
     }
 
 
